e6990ea18caed8f6756d23ee67b18be5a5ed54e4
vedants27 <vedantss27@gmail.com>
2025-04-19 19:15:08 +0530

    Added getters and setters for student fields.

--- Student.java
+++ Student.java
@@ -13,3 +13,18 @@ public class Student {
         this.batch = batch;
         this.cgpa = cgpa;
     }
+        public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public long getPRN() { return prn; }
+    public void setPRN(long prn) { this.prn = prn; }
+
+    public String getBranch() { return branch; }
+    public void setBranch(String branch) { this.branch = branch; }
+
+    public String getBatch() { return batch; }
+    public void setBatch(String batch) { this.batch = batch; }
+
+    public double getCGPA() { return cgpa; }
+    public void setCGPA(double cgpa) { this.cgpa = cgpa; }
+
