0097b668bed5aee2ad532abb7df5eb0dd50407b4
vedants27 <vedantss27@gmail.com>
2025-04-19 19:17:42 +0530

    Creayed a class for MySQL-based student operations.

--- StudentOperations.java
+++ StudentOperations.java
@@ -1,1 +1,8 @@
 //StudentOperation.java
+import java.sql.*;
+
+public class StudentOperations {
+    private Connection conn;
+    private static final String URL = "jdbc:mysql://localhost:3306/studentdb";
+    private static final String USER = "root";
+    private static final String PASSWORD = "ved@2775";
