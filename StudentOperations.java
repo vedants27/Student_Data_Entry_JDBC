9dd16c32fdb64a4466e58d434e3e5bcce004b463
vedants27 <vedantss27@gmail.com>
2025-04-19 19:18:15 +0530

    Created a constructor to establish MySQL database connection.

--- StudentOperations.java
+++ StudentOperations.java
@@ -6,3 +6,17 @@ public class StudentOperations {
     private static final String URL = "jdbc:mysql://localhost:3306/studentdb";
     private static final String USER = "root";
     private static final String PASSWORD = "ved@2775";
+
+        public StudentOperations() {
+        try {
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            if (conn != null) {
+                System.out.println("Database connection established.");
+            } else {
+                System.out.println("Failed to establish database connection.");
+            }
+        } catch (SQLException e) {
+            System.out.println("Connection error: " + e.getMessage());
+        }
+    }
+
